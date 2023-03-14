5567442b25895f5ae83c2853318894d6511ae9a5
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-03-15 00:27:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Layout from "./Components/Layout";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>This is app</h1>
+      <Layout>
+        <h1>I am talking from layout</h1>
+      </Layout>
     </div>
   );
 }
