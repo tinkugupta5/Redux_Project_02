faaaee897b986ccee921d492889ffde23f4a28a2
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-03-17 00:23:27 +0530

    adding links and redirection to link tag

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {} from "react-icons/fa";
 
 const Header = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-        <div className="container-fluid">
-          <Link className="navbar-brand" href="#">
-            Navbar
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            Cocktail Website
           </Link>
           <button
             className="navbar-toggler"
@@ -23,17 +24,21 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" href="#">
-                  Home
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/about"
+                >
+                  About
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" href="#">
+                <Link className="nav-link" to="#">
                   Link
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link disabled">Disabled</Link>
+                <Link className="nav-link ">Help</Link>
               </li>
             </ul>
             <form className="d-flex" role="search">
