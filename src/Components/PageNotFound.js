e39ce0431192f6d72ba99e8a29fa900dcb6e9c32
tinku gupta <36808633+tinkugupta5@users.noreply.github.com>
2023-03-17 20:48:45 +0530

    adding images 404 page

--- src/Components/PageNotFound.js
+++ src/Components/PageNotFound.js
@@ -2,9 +2,11 @@ import React from "react";
 import Layout from "./Layout";
 
 const PageNotFound = () => {
+  const logo = "https://fakeimg.pl/1700x820/?text=404&font=lobster";
+
   return (
     <Layout>
-      <h1>Page Not found</h1>
+      <img src={logo} alt="logo" style={{ width: "100%" }} />
     </Layout>
   );
 };
